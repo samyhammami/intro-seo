d34b6255c42868eb39f6c98017df49aac80c9bf9
Samy Hammami <samyhammami26@gmail.com>
2021-09-01 10:43:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <article>
+      <h1>Quel vélo acheté en 2021 ? - Samy Bike</h1>
+
+      <p>Très en vogue en ce moment, le vélo de voyage sert à parcourir des contrées plus ou moins lointaines. Il offre la possibilité de partir seul ou à plusieurs, pour une randonnée à vélo le temps d’une journée, d’un week-end ou de plusieurs semaines, sur des terrains variés… Selon la durée de votre voyage à vélo et votre philosophie, le deux-roues le plus approprié sera évidemment différent ! Voici 8 modèles à connaître pour bien choisir son vélo de voyage en 2021.</p>
+
+      <h2>VTT</h2>
+      <p>Conditionné pour descendre par les sentiers de montagne, le vélo tout-terrain peut désormais s’accommoder, avec quelques aménagements, à l’itinérance à vélo. Dans le marché de l’occasion, on trouve beaucoup de VTT avec un cadre acier parfaitement adaptable au cyclotourisme. </p>
+      <h2>VTC</h2>
+      <p>S’il est robuste et bien équipé, le VTC (vélo tout chemin) peut être un fidèle compagnon de voyage sur plusieurs jours, voire quelques semaines. Il faudra certainement l’améliorer avec un guidon plus fonctionnel, de nouveaux pneus, une nouvelle selle, ainsi qu’un porte-bagages approprié, mais il reste un bon compromis pour partir sur les routes à moindre coût. </p>
+      <h2>Vélo de Route</h2>
+      <p>Si le vélo de route, aussi appelé vélo de course, n’est pas toujours adapté au voyage avec du chargement, certains y parviennent très bien ! Pour ce faire, ces modèles traînent une remorque, afin d’emporter tout le nécessaire sans encombrer le vélo. Toutefois, le vélo de route reste plutôt recommandé pour les cyclistes en recherche de vitesse sur des routes bien revêtues.</p>
+      <h2>Le tandem</h2>
+      <p>Et si vous pédaliez en duo pour une expérience amusante et conviviale ? Le vélo tandem est idéal pour répartir les forces, notamment lorsque les deux cyclistes présentent des niveaux disparates ou que de jeunes enfants sont du voyage ! Notez qu’il existe des tandems semi-couchés, bon compromis pour que chacun puisse profiter du paysage.</p>
+
+
+
+    </article>
+
+
   );
 }
 
